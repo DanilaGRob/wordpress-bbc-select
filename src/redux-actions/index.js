75c7fed7e9b36082d542906494b7905cc61171a1
Danila Grobov <danila.grob@gmail.com>
2019-07-23 17:41:55 +0300

    added actions for comparisons

--- src/redux-actions/index.js
+++ src/redux-actions/index.js
@@ -11,3 +11,22 @@ export const changeType = (id, units, typeName) => ({
   type: "CHANGE_TYPE",
   props: { id, units, typeName }
 });
+export const addComp = ({
+  id = uniqid(),
+  name = null,
+  image = null,
+  amount = null,
+  type = null,
+  description = null
+}) => ({
+  type: "ADD_COMP",
+  props: { id, name, image, amount, type, description }
+});
+export const removeComp = id => ({
+  type: "REMOVE_COMP",
+  props: { id }
+});
+export const changeType = (id, name, image, amount, type, description) => ({
+  type: "CHANGE_TYPE",
+  props: { id, name, image, amount, type, description }
+});
