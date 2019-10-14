7765a9dc080511094397d9b8bd35000fbd919985
Danila Grobov <danila.grob@gmail.com>
2019-10-14 20:54:53 +0100

    asdf

--- constants.js
+++ constants.js
@@ -1,1 +1,2 @@
-export const PLUGIN_DIR = "http://dashyn-sait.local/wp-content/plugins/bbc-select-dgrobov";
+export const PLUGIN_DIR =
+  "http://draugiskigamtai.tk/wp-content/plugins/bbc-select-dgrobov";
