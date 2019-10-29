96d777aa23bc7300d8ccdcc744d4796c9e495ab5
Danila Grobov <danila.grob@gmail.com>
2019-10-29 18:22:53 +0000

    debuging

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -4,6 +4,7 @@ export default class Chart extends Component {
   formatedSelectedAmount = 0;
   mult = 1;
   getLabels = data => {
+    console.log(data[0].amount);
     if (data[0].amount * 0.25 < 1) {
       this.mult = 10;
     }
